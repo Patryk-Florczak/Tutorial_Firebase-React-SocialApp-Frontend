3a951c3f2c3fdd911b6511c84710f58df1c35b86
Patryk Florczak <projecttristis@gmail.com>
2020-09-12 21:22:44 +0200

    [add] post card component

---
+++ src/components/Scream.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const styles = {
+  card: {
+    display: 'flex',
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover',
+  },
+};
+
+class Scream extends Component {
+  render() {
+    const {
+      classes,
+      scream: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        screamId,
+        likeCount,
+        commentCount,
+      },
+    } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/user/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(Scream);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Axios from 'axios';
+
+import Scream from '../components/Scream';
 
 export class home extends Component {
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    Axios.get('/screams')
+      .then((res) => {
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <Scream scream={scream}></Scream>)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
