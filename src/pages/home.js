9e1dab9b57966f2f193b688dd760799d20fc0ba1
Patryk Florczak <projecttristis@gmail.com>
2020-09-19 01:36:35 +0200

    [add] like and unlike actions

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Axios from 'axios';
+import PropTypes from 'prop-types';
 
 import Scream from '../components/Scream';
 import Profile from '../components/Profile';
 
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
+
 export class home extends Component {
-  state = {
-    screams: null,
-  };
   componentDidMount() {
-    Axios.get('/screams')
-      .then((res) => {
-        this.setState({
-          screams: res.data,
-        });
-      })
-      .catch((err) => console.log(err));
+    this.props.getScreams();
   }
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => (
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map((scream) => (
         <Scream key={scream.screamId} scream={scream}></Scream>
       ))
     ) : (
@@ -39,4 +34,13 @@ export class home extends Component {
   }
 }
 
-export default home;
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  data: state.data,
+});
+
+export default connect(mapStateToProps, { getScreams })(home);
