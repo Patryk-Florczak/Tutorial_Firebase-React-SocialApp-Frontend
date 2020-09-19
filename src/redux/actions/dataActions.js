9fe7e6ab69dc8da698f1c3fcf105b4a44cf69fa1
Patryk Florczak <projecttristis@gmail.com>
2020-09-19 19:19:45 +0200

    [add] scream dialog

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -8,6 +8,8 @@ import {
   CLEAR_ERRORS,
   POST_SCREAM,
   SET_ERRORS,
+  SET_SCREAM,
+  STOP_LOADING_UI,
 } from '../types';
 import Axios from 'axios';
 
@@ -27,6 +29,19 @@ export const getScreams = () => (dispatch) => {
         payload: [],
       });
     });
+};
+
+export const getScream = (screamId) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  Axios.get(`/scream/${screamId}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAM,
+        payload: res.data,
+      });
+      dispatch({ type: STOP_LOADING_UI });
+    })
+    .catch((err) => console.log(err));
 };
 
 // Post a scream
